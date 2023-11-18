2acc7b62355268783f7e71e69db0224b308fafc9
Marcus Thorelius <mthorelius@gmail.com>
2023-11-18 22:08:52 +0100

    add radius

--- src/features/Blocks/BlocksPage/BlocksPage.tsx
+++ src/features/Blocks/BlocksPage/BlocksPage.tsx
@@ -1,11 +1,21 @@
 // BlocksPage.tsx
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./BlocksPage.module.css";
 
 export const BlocksPage = () => {
-  const [blockSize, setBlockSize] = useState(100); // Default size set to 100
-  const [numOfBlocks, setNumOfBlocks] = useState(200); // Default number of blocks set to 200
+  const [blockSize, setBlockSize] = useState(100);
+  const [numOfBlocks, setNumOfBlocks] = useState(200);
+  const [timeoutDuration, setTimeoutDuration] = useState(2000);
+  const [activeBlocks, setActiveBlocks] = useState<Record<number, boolean>>({});
+
   const blocksArray = Array(numOfBlocks).fill(null);
+
+  const handleMouseEnter = (index: number) => {
+    setActiveBlocks((prev) => ({ ...prev, [index]: true }));
+    setTimeout(() => {
+      setActiveBlocks((prev) => ({ ...prev, [index]: false }));
+    }, timeoutDuration);
+  };
 
   return (
     <div>
@@ -26,35 +36,68 @@ export const BlocksPage = () => {
           onChange={(e) => setNumOfBlocks(Number(e.target.value))}
         />
       </div>
+      <div>
+        <label>Timeout Duration: </label>
+        <input
+          type="number"
+          value={timeoutDuration}
+          onChange={(e) => setTimeoutDuration(Number(e.target.value))}
+        />{" "}
+        ms
+      </div>
+
       <div className={styles.container}>
         {blocksArray.map((_, index) => (
-          <Block key={index} size={blockSize} />
+          <Block
+            key={index}
+            index={index}
+            size={blockSize}
+            numOfBlocks={numOfBlocks}
+            activeBlocks={activeBlocks}
+            onMouseEnter={handleMouseEnter}
+          />
         ))}
       </div>
     </div>
   );
 };
 
-const Block = ({ size }: { size: number }) => {
-  const [isActive, setIsActive] = useState(false);
+type BlockProps = {
+  index: number;
+  size: number;
+  numOfBlocks: number;
+  activeBlocks: Record<number, boolean>;
+  onMouseEnter: (index: number) => void;
+};
 
-  useEffect(() => {
-    let timeout: NodeJS.Timeout;
-    if (isActive) {
-      timeout = setTimeout(() => {
-        setIsActive(false);
-      }, 2000);
-    }
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [isActive]);
+const Block = ({
+  index,
+  size,
+  numOfBlocks,
+  activeBlocks,
+  onMouseEnter,
+}: BlockProps) => {
+  const columns = Math.floor(window.innerWidth / size); // Assuming a full-width container
+
+  const isActive = activeBlocks[index];
+  const isLeftActive = index % columns !== 0 && activeBlocks[index - 1];
+  const isRightActive =
+    index % columns !== columns - 1 && activeBlocks[index + 1];
+  const isTopActive = index >= columns && activeBlocks[index - columns];
+  const isBottomActive =
+    index < numOfBlocks - columns && activeBlocks[index + columns];
+
+  const borderRadius =
+    (isTopActive ? "0 " : "10px ") +
+    (isRightActive ? "0 " : "10px ") +
+    (isBottomActive ? "0 " : "10px ") +
+    (isLeftActive ? "0 " : "10px ");
 
   return (
     <div
       className={isActive ? styles.activeBlock : styles.block}
-      onMouseEnter={() => setIsActive(true)}
-      style={{ width: `${size}px`, height: `${size}px` }}
+      onMouseEnter={() => onMouseEnter(index)}
+      style={{ width: `${size}px`, height: `${size}px`, borderRadius }}
     ></div>
   );
 };
